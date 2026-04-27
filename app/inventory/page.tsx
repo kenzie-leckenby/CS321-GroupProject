d5f2b35ad2f6f74ff426dd51fffdc9ad5af2bd61
Iqra Amjad <iqamjad05@gmail.com>
2026-04-26 20:07:23 -0400

    Update page.tsx

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Container, Typography } from '@mui/material';
 import PageButton from '@/components/pageButton';
 import EnhancedTable from '@/components/enhancedTable';
-
+//hello tester
 export default function Inventory() {
   return (
     <React.Fragment>
