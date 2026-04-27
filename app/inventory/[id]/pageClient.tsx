9d4ecdabc572523bd2b5a52d3b7c4e9f979e1912
Phil Aunguyen <phil.aunguyen@gmail.com>
2026-04-26 20:14:26 -0400

    Update pageClient.tsx

--- app/inventory/[id]/pageClient.tsx
+++ app/inventory/[id]/pageClient.tsx
@@ -67,9 +67,24 @@ export default function ItemPageClient({ id }: { id: string }) {
 
                     <Typography
                         variant="h5"
-                        sx={{ marginBottom: 4, marginLeft: 16, maxWidth: '75%'}}
+                        sx={{ marginBottom: 4, maxWidth: '200%', flexGrow: 1}}
                     >
-                        {description}
+                        <TextField
+                            id="filled-helperText"
+                            label="Item Description"
+                            defaultValue={description}
+                            variant="outlined"
+                            size="medium"
+                            margin="none"
+                            multiline
+                            fullWidth
+                            sx={{
+                                '& .MuiInputBase-input': { fontSize: 28 },  // input text
+                                '& .MuiInputLabel-root': { fontSize: 16 },  // label text
+                            }}
+                            onChange={(d) => setDescriptionProduct({id: product._id, newDescription: d.target.value, onUpdate: (newDescription) => setDescription(newDescription)})}
+
+                        />
                     </Typography>
                 </Container>
 
